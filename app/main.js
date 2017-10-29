004819da27cdbb9147fc56340dac3d51f94a6768
Gavin Cheng <gavincheng01@gmail.com>
2017-10-29 15:23:55 -0400

    Added comments for future reference

--- app/main.js
+++ app/main.js
@@ -5,7 +5,7 @@ var symbols = ['O', 'X'];
 var turn = 0;
 var notification = document.getElementById('notification');
 var winnerMessage = document.getElementById('winnerMessage');
-// Wais for page to load doing interesting things
+// Waits for page to load and then executes next functions
 document.onreadystatechange = function() {
 	if (document.readyState = "interactive") {
 		restartButton.onclick = startGame;
@@ -14,18 +14,14 @@ document.onreadystatechange = function() {
 };
 
 function startGame () {
-
-
 	// Reset the turn counter
 	turn = 0;
-
 	// Remove winner notification and make sure div is hidden
 	winnerMessage.innerHTML = 0;
 	notification.style.display = 'none';
-
 	// CLear board and Add click events on squares
 	for (i = 0; i < spaces.length; i++) {
-		spaces[i].innerHTML = ''
+		spaces[i].innerHTML = '';
 		spaces[i].addEventListener("click", takeSpace);
 	}
 }
@@ -33,19 +29,18 @@ function startGame () {
 function takeSpace() {
 
 	turn++;
+	// depending if 'turn' was an even or odd integer, the symbol would change between X/O.
 	var currentPlayer = symbols[turn % 2];
 	this.innerHTML = symbols[turn % 2];
 	this.removeEventListener("click", takeSpace);
-
+	// Checking for winner by refering to winable combinations from wins.js
 	for (i = 0; i < wins.length; i++) {
 		if (checkForWin(wins[i])) {
-
 			// No more clicking after win
 		for (var j = 0; j < wins.length; j++) {
 			spaces[j].removeEventListener("click", takeSpace);
 		}
-			// Notify the players
-
+			// Notify the players that the game is over
 			notification.style.display = 'block';
 			winnerMessage.innerHTML = "yayyyy!!!! " + currentPlayer + " won!!!!!!";
 		}
@@ -65,13 +60,13 @@ function takeSpace() {
 
 
 function checkForWin(winArray) {
-
-
+	// If spaces is not completely empty && 3 of the spaces in a row match the winArray with the same symbol, then there is a winner.
 	return spaces[winArray[0]].innerHTML !== '' &&
 		spaces[winArray[0]].innerHTML === spaces[winArray[1]].innerHTML &&
 		spaces[winArray[0]].innerHTML === spaces[winArray[2]].innerHTML;
 
 		}
+
 
 
 
